0f7f55d487c4620dc5b07fd093766ead76a477e2
이승윤 <syoon0624@khu.ac.kr>
2021-05-12 14:12:12 +0900

    style: image 스타일 변경

--- frontend/src/components/image.js
+++ frontend/src/components/image.js
@@ -1,42 +1,34 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
 const MainContainer = styled.div`
-    display: inline-block;
-    background-color: white;
-    justify-content: center; 
+  display: inline-block;
+  background-color: white;
+  justify-content: center;
+  width: 100%;
+  cursor: pointer;
+  overflow: hidden;
+`;
 
-    cursor: pointer;
-    border-radius:2.0em;
-    -moz-border-radius: 2.0em;
-    -webkit-border-radius: 2.0em;
-    border:1px solid #C0C0C0;
-    overflow:hidden;
-    margin-bottom : 20px;
-    min-width: 200px;
-    min-height: 100px;
-`
-
-
-const Figure = styled.div` 
-  filter:none;
-  &:hover { filter:grayscale(0.8); };
+const Figure = styled.div`
+  filter: none;
+  &:hover {
+    filter: grayscale(0.8);
+  }
 `;
 
 const Image_ = styled.img`
-  object-fit:cover;
-  width:100%;
-  vertical-align : bottom;
+  object-fit: cover;
+  width: 100%;
+  vertical-align: bottom;
 `;
 
 export default function Image(props) {
-    return (
-        <MainContainer onClick={e => props.onClickFunction(props.id)}>
-            <Figure>
-                <Image_ src = {props.src}/>
-                {props.id}
-            </Figure>
-        </MainContainer>
-    )
+  return (
+    <MainContainer onClick={e => props.onClickFunction(props.id)}>
+      <Figure>
+        <Image_ src={props.src} />
+      </Figure>
+    </MainContainer>
+  );
 }
