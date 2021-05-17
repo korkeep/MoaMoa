7a7fa5930369f80f27e1cafe3beeebcf1e59661c
이승윤 <syoon0624@khu.ac.kr>
2021-05-17 23:44:56 +0900

    feat: image 저장 버튼 추가

--- frontend/src/components/image.js
+++ frontend/src/components/image.js
@@ -13,7 +13,7 @@ const MainContainer = styled.div`
 const Figure = styled.div`
   filter: none;
   &:hover {
-    filter: grayscale(0.8);
+    filter: grayscale(0.8) blur(2px);
   }
 `;
 
@@ -22,10 +22,39 @@ const Image_ = styled.img`
   width: 100%;
   vertical-align: bottom;
 `;
-
+const DownloadButton = styled.div`
+  position: absolute;
+  z-index: 1;
+  margin-left: 10px;
+  margin-top: 10px;
+  width: 50px;
+  height: 20px;
+  font-size: 1.3em;
+  padding-left: 0.5em;
+  padding-bottom: 0.5em;
+  padding-right: 0.5em;
+  border: 2px solid #f783ac;
+  border-radius: 10px;
+  background: #f783ac;
+  text-align: center;
+  color: white;
+  cursor: default;
+  &:hover {
+    background: #e64980;
+    border: 2px solid #e64980;
+  }
+`;
 export default function Image(props) {
+  const [inHover, setHover] = useState(false);
   return (
-    <MainContainer onClick={e => props.onClickFunction(props.id)}>
+    <MainContainer
+      onClick={e => props.onClickFunction(props.id)}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      {inHover && (
+        <DownloadButton onClick="downloadFunction">저 장</DownloadButton>
+      )}
       <Figure>
         <Image_ src={props.src} />
       </Figure>
