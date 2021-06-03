a7e2b8cb26cb09dc37bf36f6386b5a31552e3782
Sungsu <korkeep@naver.com>
2021-06-03 17:02:28 +0900

    Thumbnail (#8)
    * feat: 이미지/동영상 썸네일 기능 추가
    
    * feat: 이미지/동영상 썸네일 기능 추가
    
    * feat: 이미지/동영상 썸네일 기능 추가

--- backend/Video/post_video.py
+++ backend/Video/post_video.py
@@ -3,6 +3,7 @@ import boto3
 import base64
 import boto3
 import botocore.config
+import thumbnail
 
 # POST VIDEO
 # /video/post
@@ -14,23 +15,19 @@ def post_video(cursor, conn, file, explain, singer, hashtag, date):
     # make 원본
     with open('/tmp/o_file.avi','wb') as f:
         f.write(data)
+    
     # make 썸네일
-    # 오류남
-    '''
-    t_file = Image.open('o_file.avi')
-    t_file.thumbnail(tuple(x / 2 for x in t_file.size))
-    t_file.save('t_file.png')
-    '''
-    with open('/tmp/t_file.avi','wb') as f:
-        f.write(data)
+    filename = "/tmp/o_file.avi"
+    t_file = thumbnail.create_thumbnail(filename)
+    t_file.save('/tmp/t_file.png')
 
     # upload 원본
     o_file_name = str(singer) + '_' + str(hashtag) + '_' + str(date) + '_o_file.avi'
     s3.upload_file('/tmp/o_file.avi',bucket, o_file_name)
  
     # upload 썸네일
-    t_file_name = str(singer) + '_' + str(hashtag) + '_' + str(date) + '_t_file.avi'
-    s3.upload_file('/tmp/t_file.avi',bucket, t_file_name)
+    t_file_name = str(singer) + '_' + str(hashtag) + '_' + str(date) + '_t_file.png'
+    s3.upload_file('/tmp/t_file.png',bucket, t_file_name)
 
     # s3 링크 설정
     o_link = 'https://khu-static-s3.s3.ap-northeast-2.amazonaws.com/%s' % o_file_name
